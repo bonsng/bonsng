f03577eab56833377fe7f66155349fa329a97640
bonsng <john.k7795@gmail.com>
2024-10-27 23:07:12 +0900

    dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,20 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      inset: {
+        "50vh": "50vh",
+        "50vw": "50vw",
+      },
+      transitionProperty: {
+        width: "width",
       },
     },
   },
