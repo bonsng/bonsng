c942f7afaf0f0c4f1869527e9b823cc1587c5918
bonsng <john.k7795@gmail.com>
2024-10-27 23:06:41 +0900

    Main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import MainPage3D from "@/ui/MainPage/MainPage3D";
+import MainPageHtml from "@/ui/MainPage/MainPageHtml";
+
 export default function Home() {
   return (
-    <div>
-      <div>bonsng</div>
-    </div>
+    <main className="h-[3000px]">
+      <MainPage3D />
+      <MainPageHtml />
+    </main>
   );
 }
