d2842b0e737273a954bc864618ba5d43b39a36ae
bonsng <john.k7795@gmail.com>
2024-10-27 23:04:39 +0900

    added svg/glsl plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    transpilePackages: ['three'],
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+        config.module.rules.push({
+            test: /\.(glsl|vs|fs|vert|frag|ps)$/,
+            exclude: /node_modules/,
+            use: ["raw-loader", "glslify-loader"],
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
