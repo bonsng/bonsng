080b9d8a77153d8aa6922c51c904d065ce297a41
bonsng <john.k7795@gmail.com>
2024-10-30 01:01:40 +0900

    dependencies added: postprocessing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Bonsng",
+  title: "Bonseung Koo",
   description: "Bonseung Koo Portfolio",
 };
 
@@ -13,11 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
