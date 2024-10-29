98a291183cdbcb7f9b11878b6ad352820a084ce6
bonsng <john.k7795@gmail.com>
2024-10-30 01:02:24 +0900

    cod-gray added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "cod-gray": "#131313",
       },
       inset: {
         "50vh": "50vh",
