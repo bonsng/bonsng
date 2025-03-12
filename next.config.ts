9ca5447938d571f794f606f54c0dc521997da2ca
bonsng <john.k7795@gmail.com>
2025-03-13 00:08:31 +0900

    rebuild

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["three"],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag|ps)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
